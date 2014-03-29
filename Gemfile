c78c1f150658e6c21a44206656e020c7423576e8
Kyle <ktpistor@gmail.com>
2014-03-29 15:37:38 -0700

    Finish static pages

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-ruby '2.0.0' #ruby-gemset=railstutorial_rails_4_0
+ruby '2.0.0' 
 gem 'rails', '4.0.4'
 group :development, :test do
   gem 'sqlite3', '1.3.8'
@@ -22,3 +22,4 @@ group :production do
   gem 'pg', '0.15.1'
   gem 'rails_12factor', '0.0.2'
 end
+gem 'spork', '~> 1.0rc'
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+describe "Static pages" do
+  describe "Home page" do
+    it "should have the h1 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+	end
+	    it "should have the title 'Home'" do
+	      visit '/static_pages/home'
+	      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+		end 
+	end
+
+	  describe "Help page" do
+	    it "should have the h1 'Help'" do 
+	      visit '/static_pages/help' 
+	      expect(page).to have_content('Help')
+		end
+	    it "should have the title 'Help'" do
+	      visit '/static_pages/help'
+	      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+		end 
+	end
+
+	  describe "About page" do
+	    it "should have the h1 'About Us'" do
+	      visit '/static_pages/about'
+	      expect(page).to have_content('About Us')
+		end
+	    it "should have the title 'About Us'" do
+	      visit '/static_pages/about'
+	      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+		end 
+	end
+
+	describe "Contact page" do
+	    it "should have the h1 'About Us'" do
+	      visit '/static_pages/contact'
+	      expect(page).to have_content('Contact Us')
+		end
+	    it "should have the title 'Contact Us'" do
+	      visit '/static_pages/cont'
+	      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact Us")
+		end 
+	end
+end
